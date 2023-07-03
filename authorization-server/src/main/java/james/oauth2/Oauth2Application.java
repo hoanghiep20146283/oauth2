e48f61f8a435e02ea7a2d9ead757ababcab13af3
MBAL\hieph <hiep.h@mbageas.life>
2023-07-03 17:28:58 +0700

    Update R2DB Driver MySQL

--- authorization-server/src/main/java/james/oauth2/Oauth2Application.java
+++ authorization-server/src/main/java/james/oauth2/Oauth2Application.java
@@ -1,13 +1,14 @@
 package james.oauth2;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Oauth2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Oauth2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(james.oauth2.Oauth2Application.class, args);
+    }
 
 }
